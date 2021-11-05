edda01348aae25da14c72160bfda84c9b708c4e7
zenonux@@outlook.com <zenonux@@outlook.com>
2021-11-05 18:01:47 +0800

    f

--- tests/oss.test.ts
+++ tests/oss.test.ts
@@ -2,7 +2,11 @@ import Oss from "../src/oss";
 import config from "../.deploy.config.js";
 const ossClient = new Oss(config.oss);
 describe("oss tests", () => {
-  test("sync assets", () => {
-    ossClient.sync("./dist");
+  test("sync assets", async () => {
+    try {
+      await ossClient.sync("./dist");
+    } catch (e) {
+      expect(e).toBeNull();
+    }
   });
 });
