3e575fa20d7fde2529a14e5cae9b2c822811d7a3
ZenoQi <zenonux@outlook.com>
2022-07-14 15:40:50 +0800

    test

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -1,44 +1,11 @@
+import { exec, type ExecException } from "child_process";
+
 describe("cli", () => {
-  let originalArgv: any;
-
-  beforeEach(() => {
-    // Remove all cached modules. The cache needs to be cleared before running
-    // each command, otherwise you will see the same results from the command
-    // run in your first test in subsequent tests.
-    jest.resetModules();
-
-    // Each test overwrites process arguments so store the original arguments
-    originalArgv = process.argv;
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-
-    // Set process arguments back to the original value
-    process.argv = originalArgv;
-  });
-
-  it("should run upload command", async () => {
-    const consoleSpy = jest.spyOn(console, "info");
-
-    await runCommand("upload", "stag");
-
-    expect(consoleSpy).toBeCalledWith(expect.stringContaining("success"));
+  it("should run upload command", (done: jest.DoneCallback) => {
+    const script = '"node ../dist/cli.js" upload stag';
+    exec(script, (error: ExecException | null, stdout: string) => {
+      expect(stdout).toMatch(/success/);
+      done();
+    });
   });
 });
-
-/**
- * Programmatically set arguments and execute the CLI script
- *
- * @param {...string} args - positional and option arguments for the command to run
- */
-async function runCommand(...args: any[]) {
-  process.argv = [
-    "node", // Not used but a value is required at this index in the array
-    "cli.js", // Not used but a value is required at this index in the array
-    ...args,
-  ];
-
-  // Require the yargs CLI script
-  return require("../dist/cli");
-}
