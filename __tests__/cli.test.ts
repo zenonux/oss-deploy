a11908f0fd71e49efb0b55c6ab2fe93c21769f22
ZenoQi <zenonux@outlook.com>
2022-07-14 15:04:12 +0800

    tests

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -1,33 +1,44 @@
-import OssdDeploy from "../src/index";
-import { OssOptions } from "../src/types";
-import { readJsonFile } from "../src/util";
+describe("cli", () => {
+  let originalArgv: any;
 
-const ossConfig = readJsonFile("./oss-config.json");
-const client = new OssdDeploy({
-  distPath: "./dist",
-  distFilterOptions: {},
-  packageJsonPath: "../package.json",
-  ossConfigPath: "./oss-config.json",
-  ...(ossConfig as OssOptions),
-});
+  beforeEach(() => {
+    // Remove all cached modules. The cache needs to be cleared before running
+    // each command, otherwise you will see the same results from the command
+    // run in your first test in subsequent tests.
+    jest.resetModules();
 
-describe("cli tests", () => {
-  test("upload test-stag@0.1.0", async () => {
-    try {
-      await client.uploadAssets("test", "stag", "0.4.23", false);
-    } catch (e) {
-      expect(e).toBeNull();
-    }
+    // Each test overwrites process arguments so store the original arguments
+    originalArgv = process.argv;
   });
-  // test(
-  //   "clear test-stag",
-  //   async () => {
-  //     try {
-  //       await client.clearAssets("stag");
-  //     } catch (e) {
-  //       expect(e).toBeNull();
-  //     }
-  //   },
-  //   commandWaitDelay
-  // );
+
+  afterEach(() => {
+    jest.resetAllMocks();
+
+    // Set process arguments back to the original value
+    process.argv = originalArgv;
+  });
+
+  it("should run upload command", async () => {
+    const consoleSpy = jest.spyOn(console, "info");
+
+    await runCommand("upload", "stag");
+
+    expect(consoleSpy).toBeCalledWith(expect.stringContaining("success"));
+  });
 });
+
+/**
+ * Programmatically set arguments and execute the CLI script
+ *
+ * @param {...string} args - positional and option arguments for the command to run
+ */
+async function runCommand(...args: any[]) {
+  process.argv = [
+    "node", // Not used but a value is required at this index in the array
+    "cli.js", // Not used but a value is required at this index in the array
+    ...args,
+  ];
+
+  // Require the yargs CLI script
+  return require("../dist/cli");
+}
