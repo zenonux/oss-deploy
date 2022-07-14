9b277fa32f9001bd3e003f60edbbd2f9ed79b355
换个头像心好累 <zenonux@outlook.com>
2022-07-14 19:57:26 +0800

    eslint

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -1,11 +1,11 @@
-import { exec, type ExecException } from "child_process";
+import { exec, type ExecException } from 'child_process'
 
-describe("cli", () => {
-  it("should run upload command", (done: jest.DoneCallback) => {
-    const script = '"node ../dist/cli.js" upload stag';
+describe('cli', () => {
+  it('should run upload command', (done: jest.DoneCallback) => {
+    const script = '"node ../dist/cli.js" upload stag'
     exec(script, (error: ExecException | null, stdout: string) => {
-      expect(stdout).toMatch(/success/);
-      done();
-    });
-  });
-});
+      expect(stdout).toMatch(/success/)
+      done()
+    })
+  })
+})
